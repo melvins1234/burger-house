8756941e4f5bfe8afabb9343bfc9fc01ba07a4a3
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-10-18 17:32:27 +0800

    Unfinished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Banner from "./components/Banner/Banner";
 import Middle from "./components/Middle/Middle";
 import Bottom from "./components/Bottom/Bottom";
+import OrderNowButton from "./components/atoms/Buttons/OrderNowButton/OrderNowButton";
 
 function App() {
   return (
---
+++ src/components/atoms/Buttons/OrderNowButton/OrderNowButton.jsx
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+const OrderNowButton = () => {
+  return (
+    <a className="order-now" href="#">
+      Order Now
+    </a>
+  );
+};
+
+export default OrderNowButton;
